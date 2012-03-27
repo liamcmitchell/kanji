567c56c1e9c9705b097c957b38c68b1c211d1b71
Liam Mitchell <mitchell.liam@gmail.com>
2012-03-27 19:22:17 +0900

    refactored and simplified namespacing

--- app/assets/javascripts/kanjis.js
+++ app/assets/javascripts/kanjis.js
@@ -2,183 +2,190 @@
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 
-var App = App || {};
-App.speed = 150;
-App.testingCardsSize = 7;
-App.timesToClear = 1;
-App.Card = {
-  list: {},
-  // return references to master list
-  load: function(list) {
-    K = this;
-    out = [];
-    for (i=0; i<list.length; i++) {
-      K.list[list[i]['literal']] = list[i];
-      out.push(K.list[list[i]['literal']]);
-    }
-    return out;
-  },
-  literals: function(list) {
-    out = [];
-    $.each(list, function(index, value){
-      if (value && value.literal) out.push(value.literal);
-    });
-    return out;
-  }
-};
-
-App.Tester = {
-  testingCards: [],
-  learntCards: [],
-  previousTests: [],
-  wait: false,
-  testTypes:[
+var App = function() {
+  
+  // cache app variable
+  var A = this;
+  
+  // set and localise variables
+  var speed = 150;          // base unit for animation and pause speed
+  var testingCardsSize = 7; // number of cards that will be tested at the same time
+  var timesToClear = 1;     // times each test type needs to be correct before card is learnt
+  var wait = false;         // lock
+  var canvas = null;        // container for app
+  var cards = {};           // holder for all cards used
+  var testingCards = [];    // cards currently being tested
+  var learntCards = [];     // cards tested and learnt
+  var currentTest = null;
+  var previousTests = [];
+  var testTypes = [
     {id: 1, question: 'literal',   answer: 'meaning'}, 
     {id: 2, question: 'meaning', answer: 'literal'}, 
     {id: 3, question: 'literal',   answer: 'reading'}, 
     {id: 4, question: 'reading', answer: 'literal'}
-  ],
-  init: function() {
+  ];
+  var user = {
+    loggedIn: false,
+    settings: {
+      jlpt: []
+    }
+  }
+  
+  // init
+  function init() {
     // don't start tester if not on home page
     if (window.location.pathname !== '/') return false;
     
-    T = this;
-    
     // set canvas
-    App.Canvas.canvas = $('#canvas');
+    canvas = $('#canvas');
     
     // set up keyboard listeners
-    $(document).bind('keydown.1', function(){ T.select(1); });
-    $(document).bind('keydown.2', function(){ T.select(2); });
-    $(document).bind('keydown.3', function(){ T.select(3); });
-    $(document).bind('keydown.4', function(){ T.select(4); });
+    $(document).bind('keydown.1', function(){ select(1); });
+    $(document).bind('keydown.2', function(){ select(2); });
+    $(document).bind('keydown.3', function(){ select(3); });
+    $(document).bind('keydown.4', function(){ select(4); });
     
     // load card to learn
-    if (App.User.loggedIn) {
-      T.start();
+    if (user.loggedIn) {
+      start();
     }
     else {
       // offer choices of level to start with
       d = $('<div class="dialogue" />');
-      d.append(App.Theme.title('Select a JLPT level to start with or sign in'));
-      d.append(App.Theme.box('Level 1 (hardest)', ['button']).click(function(){ App.User.settings.jlpt.push(1); T.start(); }));
-      d.append(App.Theme.box('Level 2', ['button']).click(function(){ App.User.settings.jlpt.push(2); T.start(); }));
-      d.append(App.Theme.box('Level 3', ['button']).click(function(){ App.User.settings.jlpt.push(3); T.start(); }));
-      d.append(App.Theme.box('Level 4 (easiest)', ['button']).click(function(){ App.User.settings.jlpt.push(4); T.start(); }));
-      App.Canvas.show(d);
+      d.append(themeTitle('Select a JLPT level to start with or sign in'));
+      d.append(themeBox('Level 1 (hardest)', ['button']).click(function(){ user.settings.jlpt.push(1); start(); }));
+      d.append(themeBox('Level 2', ['button']).click(function(){ user.settings.jlpt.push(2); start(); }));
+      d.append(themeBox('Level 3', ['button']).click(function(){ user.settings.jlpt.push(3); start(); }));
+      d.append(themeBox('Level 4 (easiest)', ['button']).click(function(){ user.settings.jlpt.push(4); start(); }));
+      canvasShow(d);
     }
 
-  },
-  start: function() {
-    // once testingCards is filled, start testing
-    T = this;
-    T.load(T.next);    
-  },
-  load: function(callback) {
-    T = this;
+  };
+  
+  // start
+  function start() {
+    load(function(){ next(); });
+  }
+  
+  // fill up testingCards
+  function load(callback) {
     $.ajax({
       url: '/kanjis.json',
       type: 'GET',
       data: {
-        jlpt: App.User.settings.jlpt,
-        not_in: App.Card.literals(T.testingCards).join('') + App.Card.literals(T.learntCards).join(''),
-        limit: App.testingCardsSize - T.testingCards.length,
+        jlpt: user.settings.jlpt,
+        not_in: literals(testingCards).join('') + literals(learntCards).join(''),
+        limit: testingCardsSize - testingCards.length,
         sort: 'random',
         },
       success: function(data) {
-        T.testingCards = T.testingCards.concat(App.Card.load(data));
+        testingCards = testingCards.concat(storeCards(data));
         if (callback && typeof(callback) === "function") callback();
       }
     });
-  },
-  next: function() {
-    T = App.Tester;
+  };
+  
+  // return references to master list (is this needed?)
+  function storeCards(array) {
+    out = [];
+    for (i=0; i<array.length; i++) {
+      cards[array[i]['literal']] = array[i];
+      out.push(cards[array[i]['literal']]);
+    }
+    return out;
+  };
+  
+  // next test
+  function next() {
     
-    if (T.testing) T.previousTests.push(T.testing);
+    if (currentTest) previousTests.push(currentTest);
     
     // Create new test (t)
-    T.testing = {};
-    t = T.testing;
-    t.options = T.rand(T.testingCards, 4);
-    t.card = T.rand(t.options);
+    currentTest = {};
+    t = currentTest;
+    t.options = random(testingCards, 4);
+    t.card = random(t.options);
     // don't test twice
-    if (T.previousTests.length) {
-      while (t.card == T.previousTests[T.previousTests.length - 1].testing) { 
-        t.card = T.rand(t.options);
+    if (previousTests.length) {
+      while (t.card == previousTests[previousTests.length - 1].card) { 
+        t.card = random(t.options);
       }
     }
     // choose test type from remaining choices
     if (!t.card.hasOwnProperty('results')) t.card.results = {};
     choices = [];
-    $.each(T.testTypes, function(index, value){
+    $.each(testTypes, function(index, value){
       if (!t.card.results.hasOwnProperty(value.id)) t.card.results[value.id] = 0;
-      if (t.card.results[value.id] < App.timesToClear) choices.push(value);
+      if (t.card.results[value.id] < timesToClear) choices.push(value);
     });
-    t.type = T.rand(choices);
+    t.type = random(choices);
     t.result = null;
     
     // empty jquery object to hold content we will eventually display on the canvas
     c = $('<div class="test" />');
-
-    c.append(App.Theme.card(t.card, 'question', t.type));
-    
+    // add question card
+    c.append(themeCard(t.card, 'question', t.type));
+    // add options to choose from
     options = $('<div class="options" />');
     for (i=0; i<t.options.length; i++) {
-      option = $(App.Theme.card(t.options[i], 'answer', t.type)).click(function() {
-        T.select(this);
+      option = $(themeCard(t.options[i], 'answer', t.type)).click(function() {
+        select($(this));
       });
       options.append(option);
     }
     c.append(options);
     
-    App.Canvas.show(c);
+    // finally show new test on the canvas
+    canvasShow(c);
     
-  },
+  };
   
   // returns random pieces from array
-  rand: function(array, number) {
+  function random(array, number) {
     if (!number) number = 1;
     if (number > array.length) number = array.length;
     if (number == 1) return array[Math.floor(Math.random() * array.length)];
-    else return this.shuffle(array).slice(0, number);
-  },
+    else return shuffle(array).slice(0, number);
+  };
   
   // shuffles array, taken from SO
-  shuffle: function (array) {
+  function shuffle(array) {
     var tmp, current, top = array.length;
-
     if (top) while(--top) {
         current = Math.floor(Math.random() * (top + 1));
         tmp = array[current];
         array[current] = array[top];
         array[top] = tmp;
     }
-
     return array;
-  },
+  };
+  
+  // return array of literals from given cards
+  function literals(set) {
+    out = [];
+    $.each(set, function(index, value){
+      if (value && value.literal) out.push(value.literal);
+    });
+    return out;
+  }
   
   // handler for answer selection
-  select: function(answer) {
-    T = App.Tester;
-    t = T.testing;
+  function select(answer) {
+    t = currentTest;
     
-    // keyboard
-    if (typeof(answer) == 'number') {
-      answer = App.Canvas.canvas.find('.answer')[answer - 1];
-    }
-    
-    answer = $(answer);
+    // handle keyboard selection 
+    if (typeof(answer) == 'number') answer = canvas.find('.answer').eq(answer - 1);
     
     // if no answer quietly fail
     if (!answer.length) return false;
     
-    // if correct card selected (may not be 1st guess)
+    // if correct card was selected (may not be 1st guess)
     if (answer.data('card') == t.card) {
       answer.addClass('correct');
       
-      // wait prevents double action while in transition
-      if (!T.wait) {
-        T.wait = true;
+      // lock prevents double action while in transition
+      if (!wait) {
+        wait = true;
         
         // if correct card was selected first
         if (t.result === null) {
@@ -188,26 +195,24 @@ App.Tester = {
           
           // if card has passed requirements, record card as learnt
           learnt = true;
-          console.log(T.testTypes);
-          console.log(t.card.results);
-          $.each(T.testTypes, function(index, value){
-            if (t.card.results[value.id] < App.timesToClear) learnt = false;
+          $.each(testTypes, function(index, value){
+            if (t.card.results[value.id] < timesToClear) learnt = false;
           });
           if (learnt) {
             // remove from testingCards
-            T.testingCards.splice(T.testingCards.indexOf(t.card), 1);
-            T.learntCards.push(t.card);
+            testingCards.splice(testingCards.indexOf(t.card), 1);
+            learntCards.push(t.card);
             // post to server
             
             // show user
             $('#learnt').append(t.card.literal).show();
             // get new cards
-            T.load();
+            load();
           }
         }
         
         // continue to next test
-        setTimeout(T.next, App.speed);
+        setTimeout(function(){ next(); }, speed);
       }
     }
     
@@ -224,20 +229,24 @@ App.Tester = {
       else answer.removeClass(t.type.question).addClass(t.type.answer);
       
     }
-  }
-};
-App.Theme = {
-  box: function(content, classes) {
+  };
+  
+  // theme a box div
+  function themeBox(content, classes) {
     if (classes) classes = classes.join(' ');
     else classes = '';
     return $('<div class="box ' + classes + '"></div>').append(content);
-  },
-  title: function(content, classes) {
+  };
+  
+  // theme a title
+  function themeTitle(content, classes) {
     if (classes) classes = classes.join(' ');
     else classes = '';
     return $('<h2 class="' + classes + '"></h2>').append(content);
-  },
-  card: function(card, qa, type) {
+  };
+  
+  // theme a card
+  function themeCard(card, qa, type) {
     // make verb stem bold
     kunyomi = card.kunyomi.split(', ');
     for (var i = 0; i < kunyomi.length; i++) {
@@ -250,37 +259,33 @@ App.Theme = {
       + '<div class="meaning">' + card.meaning + '</div>'
       + '<div class="onyomi">' + card.onyomi + '</div>'
       + '<div class="kunyomi">' + (kunyomi.join(', ') || '') + '</div>';
-    return App.Theme.box(html, [qa, type[qa]]).data('card', card);
-  },
-};
-App.Canvas = {
-  canvas: null, // set on init
-  show: function(content) {
-    c = this.canvas;
-    App.Tester.wait = true;
-    App.Animate.hide(c, function(){
-      c.empty().append(content);
+    return themeBox(html, [qa, type[qa]]).data('card', card);
+  };
+  
+  // show new content on canvas
+  function canvasShow(content) {
+    wait = true;
+    hide(canvas, function(){
+      canvas.empty().append(content);
       setTimeout(function(){
-        App.Tester.wait = false;
-        App.Animate.show(c);  
-      }, App.speed);
+        wait = false;
+        show(canvas);  
+      }, speed);
     });
-  }
-};
-// remove animate later if only used for canvas transition
-App.Animate = {
-  hide: function(object, callback) {
-    object = $(object);
-    object.css({position: 'relative'}).animate({opacity:0, left: '-100px'}, App.speed, 'swing', callback);
-  },
-  show: function(object, callback) {
-    object = $(object);
-    object.css({position: 'relative', left: '100px'}).animate({opacity: 1, left: '0'}, App.speed, 'swing', callback);
-  }
-};
-App.User = {
-  loggedIn: false,
-  settings: {
-    jlpt: []
-  }
+  };
+  
+  // hide animation
+  function hide(object, callback) {
+    object.css({position: 'relative'}).animate({opacity:0, left: '-100px'}, speed, 'swing', callback);
+  };
+  
+  // show animation
+  function show(object, callback) {
+    object.css({position: 'relative', left: '100px'}).animate({opacity: 1, left: '0'}, speed, 'swing', callback);
+  };
+  
+  init();
+  
+  // return object
+  return this;
 };
--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -2,6 +2,6 @@
 
 $(document).ready(function() {
   
-  App.Tester.init();
-  console.log(App);
+  App();
+  
 });
